11aee7d0e235a2022b796d0781a325ac6fe84cdb
Kyoungsik Choi <kyoungsik.dev@gmail.com>
2019-11-17 20:08:19 +0900

    불필요한 dependency, 파일 제거

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import Root from './Root';
+import App from './App';
 
-ReactDom.render(<Root />, document.getElementById('root'));
+ReactDom.render(<App />, document.getElementById('root'));
