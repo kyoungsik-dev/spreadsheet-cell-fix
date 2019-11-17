b4534e38d7a3169dec171628ecf074926431670e
Kyoungsik Choi <kyoungsik.dev@gmail.com>
2019-11-17 19:54:04 +0900

    스크롤 기능 함수로 분리

--- src/App.js
+++ src/App.js
@@ -17,56 +17,64 @@ class App extends Component {
       table4_pos: {x: 0, y: 0}
     };
     this.setFixedPos = this.setFixedPos.bind(this);
+    this.scrollHorizontal = this.scrollHorizontal.bind(this);
+    this.scrollVertical = this.scrollVertical.bind(this);
   }
   componentDidMount() {
     const parseData = csv.split('\n').map(o => o.split(','));
     this.setState({size: {x: parseData[0].length, y: parseData.length}});
 
-    const table1_dom = document.getElementById('table_wrapper_1');
-    const table3_dom = document.getElementById('table_wrapper_3');
-    const table4_dom = document.getElementById('table_wrapper_4');
+    this.table1_dom = document.getElementById('table_wrapper_1');
+    this.table3_dom = document.getElementById('table_wrapper_3');
+    this.table4_dom = document.getElementById('table_wrapper_4');
 
     let prevEventTime = 0;
     document.addEventListener('mousewheel', (e) => {
-      console.log(e);
-      //if (e.timeStamp - prevEventTime > 5) {
+      if (e.timeStamp - prevEventTime > 70) {
         prevEventTime = e.timeStamp;
-
         const [dx, dy] = [e.wheelDeltaX, e.wheelDeltaY];
 
         if (Math.abs(dx) > Math.abs(dy)) { //가로 움직임의 경우 1,4분면 이동
-          const way = (dx < 0) ? -1 : 1; //오른쪽 스크롤 : -1, 왼쪽 스크롤 : 1
-
-          //오른쪽 스크롤 - 왼쪽으로 더 못감
-          if (dx < 0 && this.state.table1_pos.x+1*way < (this.state.size.x - this.state.fixedPos.x)*(-1) + 1) return;
-
-          //왼쪽 스크롤 - 오른쪽으로 더 못감
-          if (dx > 0 && this.state.table1_pos.x+1*way > 0) return;
-
-          table1_dom.style.transform = `translateX(${(this.state.table1_pos.x+1*way)*100}px)`;
-          this.setState({table1_pos: {x: this.state.table1_pos.x+1*way, y: this.state.table1_pos.y}});
-
-          table4_dom.style.transform = `translate(${(this.state.table4_pos.x+1*way)*100}px, ${this.state.table4_pos.y*34}px)`;
-          this.setState({table4_pos: {x: this.state.table4_pos.x+1*way, y: this.state.table4_pos.y}});
+          this.scrollHorizontal(dx);
         }
 
         else { //세로 움직임의 경우 3,4분면 이동
-          const way = (dy < 0) ? -1 : 1; //아래쪽 스크롤 : -1, 위쪽 스크롤 : 1
-
-          //아래쪽 스크롤 - 위로 더 못감
-          if (dy < 0 && this.state.table3_pos.y+1*way < (this.state.size.y - this.state.fixedPos.y)*(-1) + 1) return;
-
-          //위쪽 스크롤 - 아래쪽으로 더 못감
-          if (dy > 0 && this.state.table3_pos.y+1*way > 0) return;
-
-          table3_dom.style.transform = `translateY(${(this.state.table3_pos.y+1*way)*34}px)`;
-          this.setState({table3_pos: {x: this.state.table3_pos.x, y: this.state.table3_pos.y+1*way}});
-
-          table4_dom.style.transform = `translate(${this.state.table4_pos.x*100}px, ${(this.state.table4_pos.y+1*way)*34}px)`;
-          this.setState({table4_pos: {x: this.state.table4_pos.x, y: this.state.table4_pos.y+1*way}});
+          this.scrollVertical(dy);
         }
-      //}
+      }
     });
+  }
+
+  scrollHorizontal(dx) {
+    const way = (dx < 0) ? -1 : 1; //오른쪽 스크롤 : -1, 왼쪽 스크롤 : 1
+
+    //오른쪽 스크롤 - 왼쪽으로 더 못감
+    if (dx < 0 && this.state.table1_pos.x+1*way < (this.state.size.x - this.state.fixedPos.x)*(-1) + 1) return;
+
+    //왼쪽 스크롤 - 오른쪽으로 더 못감
+    if (dx > 0 && this.state.table1_pos.x+1*way > 0) return;
+
+    this.table1_dom.style.transform = `translateX(${(this.state.table1_pos.x+1*way)*100}px)`;
+    this.setState({table1_pos: {x: this.state.table1_pos.x+1*way, y: this.state.table1_pos.y}});
+
+    this.table4_dom.style.transform = `translate(${(this.state.table4_pos.x+1*way)*100}px, ${this.state.table4_pos.y*34}px)`;
+    this.setState({table4_pos: {x: this.state.table4_pos.x+1*way, y: this.state.table4_pos.y}});
+  }
+
+  scrollVertical(dy) {
+    const way = (dy < 0) ? -1 : 1; //아래쪽 스크롤 : -1, 위쪽 스크롤 : 1
+
+    //아래쪽 스크롤 - 위로 더 못감
+    if (dy < 0 && this.state.table3_pos.y+1*way < (this.state.size.y - this.state.fixedPos.y)*(-1) + 1) return;
+
+    //위쪽 스크롤 - 아래쪽으로 더 못감
+    if (dy > 0 && this.state.table3_pos.y+1*way > 0) return;
+
+    this.table3_dom.style.transform = `translateY(${(this.state.table3_pos.y+1*way)*34}px)`;
+    this.setState({table3_pos: {x: this.state.table3_pos.x, y: this.state.table3_pos.y+1*way}});
+
+    this.table4_dom.style.transform = `translate(${this.state.table4_pos.x*100}px, ${(this.state.table4_pos.y+1*way)*34}px)`;
+    this.setState({table4_pos: {x: this.state.table4_pos.x, y: this.state.table4_pos.y+1*way}});
   }
 
   setFixedPos(o) {
@@ -77,6 +85,7 @@ class App extends Component {
       alert("입력 값이 유효하지 않습니다.");
     }
   }
+
   render() {
     const parseData = csv.split('\n').map(o => o.split(','));
     const {x, y} = this.state.fixedPos;
